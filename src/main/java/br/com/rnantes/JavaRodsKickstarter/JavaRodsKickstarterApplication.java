09fc27b2efe2d393b7ce15092bbf73e213a3f0cc
rodtwo <rodtwo@hotmail.com>
2018-03-21 09:38:01 -0300

    RxJava; setups

--- src/main/java/br/com/rnantes/JavaRodsKickstarter/JavaRodsKickstarterApplication.java
+++ src/main/java/br/com/rnantes/JavaRodsKickstarter/JavaRodsKickstarterApplication.java
@@ -2,11 +2,24 @@ package br.com.rnantes.JavaRodsKickstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import io.reactivex.*;
 
-@SpringBootApplication
+/* The 'exclude' arguments below are for disabling database on start - remove when needed
+ */
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class, 
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        MongoRepositoriesAutoConfiguration.class, })
 public class JavaRodsKickstarterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaRodsKickstarterApplication.class, args);
+		Flowable.just("Hello sick sad wurld").subscribe(System.out::println);
 	}
 }
