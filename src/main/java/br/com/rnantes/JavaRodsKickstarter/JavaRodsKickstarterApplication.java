7f1f8a9764ffdf12b446f316d04f180fda8c039f
rodtwo <rodtwo@hotmail.com>
2018-03-21 08:40:41 -0300

    Kickstarter Start

---
+++ src/main/java/br/com/rnantes/JavaRodsKickstarter/JavaRodsKickstarterApplication.java
@@ -0,0 +1,12 @@
+package br.com.rnantes.JavaRodsKickstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRodsKickstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRodsKickstarterApplication.class, args);
+	}
+}
